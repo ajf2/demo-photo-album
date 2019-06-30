ec17dafc5319edec3e5d9e19cd4250354e78fd29
ajf2 <ajf2@users.noreply.github.com>
2019-06-30 21:56:51 +0100

    Formatting.

--- DemoPhotoAlbum/Controllers/AlbumsController.cs
+++ DemoPhotoAlbum/Controllers/AlbumsController.cs
@@ -5,24 +5,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DemoPhotoAlbum.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AlbumsController : ControllerBase
-    {
-        // GET: api/Albums
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Albums/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+namespace DemoPhotoAlbum.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AlbumsController : ControllerBase {
+    // GET: api/Albums
+    [HttpGet]
+    public IEnumerable<string> Get() {
+      return new string[] { "value1", "value2" };
     }
+
+    // GET: api/Albums/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id) {
+      return "value";
+    }
+  }
 }
