da70496b073b857403e320f5515cc9e691415402
ajf2 <ajf2@users.noreply.github.com>
2019-06-30 22:10:07 +0100

    Added simple models.

---
+++ DemoPhotoAlbum/Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoPhotoAlbum.Models {
+  public class Photo {
+    public int AlbumId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Uri Url { get; set; }
+    public Uri ThumbnailUrl { get; set; }
+  }
+}
