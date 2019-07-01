daf5675c159b354bf7f21334f1e5e6ff2ed311da
ajf2 <ajf2@users.noreply.github.com>
2019-07-01 15:46:02 +0100

    Added (de)serialisation attributes to models.

--- DemoPhotoAlbum/Models/Album.cs
+++ DemoPhotoAlbum/Models/Album.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace DemoPhotoAlbum.Models {
+  [DataContract]
   public class Album {
+    [DataMember(Name = "userId")]
     public int UserId { get; set; }
+    [DataMember(Name = "id")]
     public int Id { get; set; }
+    [DataMember(Name = "title")]
     public string Title { get; set; }
+    [DataMember(Name = "photos")]
+    public IEnumerable<Photo> Photos { get; set; }
   }
 }
--- DemoPhotoAlbum/Models/Photo.cs
+++ DemoPhotoAlbum/Models/Photo.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace DemoPhotoAlbum.Models {
+  [DataContract]
   public class Photo {
+    [DataMember(Name = "albumId")]
     public int AlbumId { get; set; }
+    [DataMember(Name = "id")]
     public int Id { get; set; }
+    [DataMember(Name = "title")]
     public string Title { get; set; }
+    [DataMember(Name = "url")]
     public Uri Url { get; set; }
+    [DataMember(Name = "thumbnailUrl")]
     public Uri ThumbnailUrl { get; set; }
   }
 }
