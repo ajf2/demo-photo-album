e7478042815d7cd4931408bc7dc4d8509de306c3
ajf2 <ajf2@users.noreply.github.com>
2019-07-01 21:59:25 +0100

    Added a first test.

--- DemoPhotoAlbum.Tests/PhotoAlbumsControllerTests.cs
+++ DemoPhotoAlbum.Tests/PhotoAlbumsControllerTests.cs
@@ -1,11 +1,62 @@
+using DemoPhotoAlbum.Controllers;
+using DemoPhotoAlbum.Models;
+using DemoPhotoAlbum.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DemoPhotoAlbum.Tests {
   [TestClass]
   public class PhotoAlbumsControllerTests {
-    //[TestMethod]
-    //public Task GetAsync_ReturnsPhotoAlbums_WithPhotosPropertyPopulated() {
-    //}
+    private IEnumerable<PhotoAlbum> MockData => new List<PhotoAlbum> {
+      new PhotoAlbum {
+        Id = 1,
+        Photos = new List<Photo> {
+          
+        },
+        Title = "First User's First Mock Album",
+        UserId = 1
+      },
+      new PhotoAlbum {
+        Id = 2,
+        Photos = new List<Photo> {
+
+        },
+        Title = "First User's Second Mock Album",
+        UserId = 1
+      },
+      new PhotoAlbum {
+        Id = 3,
+        Photos = new List<Photo> {
+
+        },
+        Title = "Second User's First Mock Album",
+        UserId = 2
+      },
+      new PhotoAlbum {
+        Id = 4,
+        Photos = new List<Photo> {
+
+        },
+        Title = "Second User's Second Mock Album",
+        UserId = 2
+      },
+    };
+
+    [TestMethod]
+    public async Task GetAsync_ReturnsPhotoAlbums_WithPhotosPropertyPopulated() {
+      var mockRepo = new Mock<IPhotoAlbumRepository>();
+      var userId = 1;
+      mockRepo
+        .Setup(repo => repo.GetPhotoAlbumsAsync(userId))
+        .ReturnsAsync(MockData.Where(a => a.UserId == userId));
+      var controller = new PhotoAlbumsController(mockRepo.Object);
+
+      var result = await controller.GetAsync(userId);
+
+      Assert.AreEqual(2, result.Count());
+    }
   }
 }
