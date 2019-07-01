b790b0bb75da49a96f80068dc71f18cd8692ebae
ajf2 <ajf2@users.noreply.github.com>
2019-07-01 19:12:27 +0100

    Added a basic test project.

---
+++ DemoPhotoAlbum.Tests/PhotoAlbumsControllerTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace DemoPhotoAlbum.Tests {
+  [TestClass]
+  public class PhotoAlbumsControllerTests {
+    //[TestMethod]
+    //public Task GetAsync_ReturnsPhotoAlbums_WithPhotosPropertyPopulated() {
+    //}
+  }
+}
